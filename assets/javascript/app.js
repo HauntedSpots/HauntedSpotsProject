bdd84188eb032cc0eeabd0bb9353aa2c696535af
Curtis Lytle <curtlytle@gmail.com>
2017-11-01 19:36:48 -0600

    Curts testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,4 +2,4 @@ console.log("Hello Haunted World");
 console.log("Curts Hello Haunted World");
 console.log("Hello World");
 console.log("change one for jonsDevelop Branch");
-console.log("Hello World Two");
+console.log("Hello World Three more");
