e270d873342f2542f8868077c9043b057e19dae8
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2017-11-01 19:50:37 -0600

    clear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,1 @@
-console.log("Hello Haunted World");
-console.log("Curts Hello Haunted World");
-console.log("Hello World");
-console.log("change one for jonsDevelop Branch");
-console.log("Hello World Three more");
-console.log("Hello World Two");
-console.log("Jon's change five");
+console.log("clear");
