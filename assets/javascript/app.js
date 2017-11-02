174f1568ec888986fb5035d906cc3301046b0b64
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2017-11-01 20:28:59 -0600

    firebase and google api key added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,15 @@
-console.log("clear");
-console.log("clear 2");
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAcdqr-aT41H0CEvg5-_t6_ldpMHKeYmM4",
+	authDomain: "haunted-spots-1509589532630.firebaseapp.com",
+	databaseURL: "https://haunted-spots-1509589532630.firebaseio.com",
+	projectId: "haunted-spots-1509589532630",
+	storageBucket: "",
+	messagingSenderId: "378273039449"
+};
+
+firebase.initializeApp(config);
+
+var db = firebase.database();
+
+var googleMapsApiKey = "AIzaSyA45PlRWp7d6LyTh9EPb6yF8zPT98K1e78";
