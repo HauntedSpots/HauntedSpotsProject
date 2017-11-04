30a87751ad2c9f049f5772f014aaad755f764467
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2017-11-04 17:24:35 -0600

    added fader

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,6 @@ var config = {
 firebase.initializeApp(config);
 
 var db = firebase.database();
-var totalChildren = 0;
 
 var googleMapsApiKey = "AIzaSyDY7MH2dv1jH8-T__4VIShSb79MOxirXLM";
 
@@ -22,48 +21,53 @@ function initMap() {
       center: {lat: 40.7607793, lng: -111.8910474}
     });
 
+   	$(".fader").on("mouseenter", function() {
+   		$(this).fadeTo("fast", 1);
+   	});
+   	$(".fader").on("mouseleave", function() {
+   		$(this).fadeTo("slow", 0);
+   	});
    	db.ref("/stories").on("child_added", function(snapshot) { 
-		totalChildren++;
 
-			var date = snapshot.val().date;
-			var description = snapshot.val().description;
-			var locationCity = snapshot.val().location.city;
-			var locationGeoLatitude = snapshot.val().location.geo.latitude;
-			var locationGeoLongitude = snapshot.val().location.geo.longitude;
-			var locationState = snapshot.val().location.state;
-			var name = snapshot.val().name;
-			var rating = String(snapshot.val().rating);
-			var truthFactor	= snapshot.val().truthFactor;
-			var type = snapshot.val().type;
+		var date = snapshot.val().date;
+		var description = snapshot.val().description;
+		var locationCity = snapshot.val().location.city;
+		var locationGeoLatitude = snapshot.val().location.geo.latitude;
+		var locationGeoLongitude = snapshot.val().location.geo.longitude;
+		var locationState = snapshot.val().location.state;
+		var name = snapshot.val().name;
+		var rating = String(snapshot.val().rating);
+		var truthFactor	= snapshot.val().truthFactor;
+		var type = snapshot.val().type;
 
-			var newPoint = new google.maps.Marker({
-			    position: {lat: locationGeoLatitude, lng: locationGeoLongitude},
-			    map: map,
-			    label: rating
-			    //icon: 
-			});
+		var newPoint = new google.maps.Marker({
+		    position: {lat: locationGeoLatitude, lng: locationGeoLongitude},
+		    map: map,
+		    label: rating
+		    //icon: 
+		});
 
-			var hoverwindow = new google.maps.InfoWindow({
-			  	content: "<div>" + name + "</div>"
-			})
+		var hoverwindow = new google.maps.InfoWindow({
+		  	content: "<div>" + name + "</div>"
+		})
 
-		    newPoint.addListener('click', function() {
-		    	$("#pointTitle").text(name);
+	    newPoint.addListener('click', function() {
+	    	$("#pointTitle").text(name);
 
-		    	var newDiv = $("<div>");
-		    	newDiv.append($("<p>").text("Location: " + locationCity + ", " + locationState));
-		    	newDiv.append($("<p>").text("Type: " + type));
-		    	newDiv.append($("<p>").text("Rating: " + rating));
-		    	newDiv.append($("<p>").text("Description: " + description));
-		    	$("#pointDetails").html(newDiv);
-		    	$("#myModal").modal("show");
-		    	hoverwindow.close(map, newPoint);
-		  	});
-		    newPoint.addListener('mouseover', function() {
-			    hoverwindow.open(map, newPoint);
-		  	});
-		  	newPoint.addListener('mouseout', function() {
-		  		hoverwindow.close(map, newPoint);
-		  	});
+	    	var newDiv = $("<div>");
+	    	newDiv.append($("<p>").text("Location: " + locationCity + ", " + locationState));
+	    	newDiv.append($("<p>").text("Type: " + type));
+	    	newDiv.append($("<p>").text("Rating: " + rating));
+	    	newDiv.append($("<p>").text("Description: " + description));
+	    	$("#pointDetails").html(newDiv);
+	    	$("#myModal").modal("show");
+	    	hoverwindow.close(map, newPoint);
+	  	});
+	    newPoint.addListener('mouseover', function() {
+		    hoverwindow.open(map, newPoint);
+	  	});
+	  	newPoint.addListener('mouseout', function() {
+	  		hoverwindow.close(map, newPoint);
+	  	});
 	});
 };
