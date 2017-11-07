d43562d5abfa637d57fb670a66e909064ac0d46d
Curtis Lytle <curtlytle@gmail.com>
2017-11-06 19:55:10 -0700

    Adding data to Firebase.  Yippee

--- assets/javascript/addSiting.js
+++ assets/javascript/addSiting.js
@@ -12,19 +12,63 @@ var database = firebase.database();
 var storiesDB = database.ref("/stories");
 var stories = [];
 
-storiesDB.on("value", function (snapshot) {
-    var data = snapshot.val();
-    if (data === null) {
-        stories = [];
-        return;  // no data, just return
+// storiesDB.on("value", function (snapshot) {
+//     var data = snapshot.val();
+//     if (data === null) {
+//         // stories = [];
+//         return;  // no data, just return
+//     }
+//     console.log("In Value from storiesDB on value.... " + data);
+//
+//
+// }, function (errorObject) {
+//     console.log("The read failed: " + errorObject.code);
+// });
+
+var loadedFromFirebaseAlready = false;
+storiesDB.on("child_added", function(snapshot) {
+    if (loadedFromFirebaseAlready) {
+        console.log("Already loaded, no need to load again: " + snapshot.val());
+        return;
     }
-    stories = data;
-    console.log(stories[0].description);
+    var data = snapshot.val();
+    console.log("Data from child_added: ");
+    console.log(data);
+    var date = data.date;
+    var description = data.description;
+    var city = data.location.city;
+    var latitude = data.location.geo.latitude;
+    var longitude = data.location.geo.longitude;
+    var state = data.location.state;
+    var name = data.name;
+    var rating = String(data.rating);
+    var truthFactor	= data.truthFactor;
+    var type = data.type;
+
+    var story = {
+        name: name,
+        location: {
+            state: state,
+            city: city,
+            geo: {
+                longitude: longitude,
+                latitude: latitude
+            }
+        },
+        description: description,
+        type: type,
+        rating: rating,
+        truthFactor: truthFactor,
+        date: date
+    };
+
+    stories.push(story);
 
 
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
+
 
 function createStory1() {
     var story = {
@@ -56,8 +100,8 @@ function createStory2() {
             state: "UT",
             city: "Kaysville",
             geo: {
-                longitude: 180,
-                latitude: 555
+                longitude: -111.9385521,
+                latitude: 41.0352216
             }
         },
         description: "Testing the Kaysville Ghost and the Kaysville residents",
@@ -76,5 +120,41 @@ function loadup(){
     createStory2();
     storiesDB.set(stories);
 }
+
+$("#add-siting-btn").on("click", function () {
+    event.preventDefault();
+    loadedFromFirebaseAlready = true;
+    var storyName = $("#name-input").val().trim();
+    var storyDate = $("#date-input").val().trim();
+    var storyDesc = $("#desc-input").val().trim();
+    var storyLongitude = Number($("#longitude").val().trim());
+    var storyLatitude = Number($("#latitude").val().trim());
+    var storyState = $("#state").val().trim();
+    var storyCity = $("#city").val().trim();
+    var storyType = $("#type").val().trim();
+    var storyRating = Number($("#rating").val().trim());
+
+    var story = {
+        name: storyName,
+        location: {
+            state: storyState,
+            city: storyCity,
+            geo: {
+                longitude: storyLongitude,
+                latitude: storyLatitude
+            }
+        },
+        description: storyDesc,
+        type: storyType,
+        rating: storyRating,
+        truthFactor: 1,
+        date: storyDate
+    };
+
+    stories.push(story);
+
+    storiesDB.set(stories);
+    // console.log(story);
+});
 
 // loadup();
