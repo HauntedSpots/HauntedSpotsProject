0b329ce23cf648265f5e58847911addb7071021f
Curtis Lytle <curtlytle@gmail.com>
2017-11-08 19:46:06 -0700

    Adding geo help and house marker image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -141,6 +141,25 @@ function initMap() {
                 animation: google.maps.Animation.DROP,
                 icon: image
             });
+        }else if (type === 'House') {
+                var image = {
+                    url: 'assets/images/house.png',
+                    // This marker is 20 pixels wide by 32 pixels high.
+                    size: new google.maps.Size(50, 62),
+                    // The origin for this image is (0, 0).
+                    origin: new google.maps.Point(0, 0),
+                    // The anchor for this image is the base of the flagpole at (0, 32).
+                    anchor: new google.maps.Point(0, 32)
+                };
+
+
+                newPoint = new google.maps.Marker({
+                    position: {lat: locationGeoLatitude, lng: locationGeoLongitude},
+                    map: map,
+                    id: locationCity,
+                    animation: google.maps.Animation.DROP,
+                    icon: image
+                });
         } else {
             newPoint = new google.maps.Marker({
                 position: {lat: locationGeoLatitude, lng: locationGeoLongitude},
